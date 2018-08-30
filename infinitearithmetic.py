9fe08d9f6633963f78f61f8b31f2941e6f806197
ornelasf1 <ornelasf1@gmail.com>
2018-08-30 14:37:54 -0500

    added writeline

--- infinitearithmetic.py
+++ infinitearithmetic.py
@@ -3,5 +3,6 @@ from file_maker import *
 inputTxt = open("input.txt", "r")
 getLines(inputTxt)
 print(lines)
+writeLines("out.txt", lines)
 
 #this file is for the main part evaluate arithmetic operators
