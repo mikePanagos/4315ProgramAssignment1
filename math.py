0234f76a83abb208e7fe9aaaa92531d093e9ba23
Michael Panagos <michael@technicool.com>
2018-09-11 13:13:41 -0500

    added carry

--- math.py
+++ math.py
@@ -19,8 +19,11 @@ def setlists():
 def mathAdd(list1,list2 ,carry,index):
     # print("carry is ",carry)
     adding=int(list1[index])+int(list2[index])+int(carry)
+    
 
     add=str(adding)
+    if(add=="0"):
+        add="0000"
 
     # check if the 2 segments added together is bigger then the 4 digit segment allowed if so add the last digit to carry
     if(len(add)>4):
@@ -28,6 +31,8 @@ def mathAdd(list1,list2 ,carry,index):
         # print( "carry is =",carry)
         final.append(add[1:])
         if (index == len(list1)-1):
+            if(carry):
+                final.append(carry)
             return 0
         mathAdd(list1,list2,carry,index+1)
     else:
@@ -60,8 +65,9 @@ def formlists(list,number):
     formlists(list,number[:-4])
     
 # x="199991111"
-# print(x[-4:])
-formlists(list1,"199991111")
+# print(x[-4:])            [2]
+# 1000 +9000=  [1][0000][0000]
+formlists(list1,"100001111")
 print(list1)
 formlists(list2,"111110000")
 print(list2)
@@ -71,9 +77,9 @@ print(assemble(0))
 
 setlists()
 print("should be 6")
-formlists(list1,"3")
+formlists(list1,"1000")
 print(list1)
-formlists(list2,"3")
+formlists(list2,"9000")
 print(list2)
 mathAdd(list1,list2,0,0)
 print("=")
