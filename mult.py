19e1e75c53944658ca279477050bd500fda622b3
Michael Panagos <michael@technicool.com>
2018-09-15 11:30:29 -0500

    still needs some work but almost done with mult

--- mult.py
+++ mult.py
@@ -1,94 +1,62 @@
-# list1 = list()
-# list2 = list()
-# final = list()
+# takes 2 lists goes through them and adds them together in segments if the string is bigger then the
+# segment it will return it as a carry to add onto the next
 
 
-# print("shoud = 311101111")
+def mathAdd(list, list1, list2, carry, index):
+    # print("carry is ",carry)
+    if(len(list1) < 0):
+        adding = int(list2[index])+int(carry)
+    if(len(list2) < 0):
+        adding = int(list1[index])+int(carry)
+    else:
+        adding = int(list1[index])+int(list2[index])+int(carry)
 
-# # this is a test
-# def setlists():
-#     del list1[:]
-#     del list2[:]
-#     del final[:]
+    add = str(adding)
+    # print (add)
+    if(add == "0"):
+        add = "0000"
 
+    # check if the 2 segments added together is bigger then the 4 digit segment allowed if so add the last digit to carry
+    if(len(add) > 4):
+        carry = int(add[:1])
+        # print( "carry is =",carry)
+        list.append(add[1:])
+        # print ("the list is",list)
+        if (index == len(list1)-1):
+            if(carry):
+                list.append(str(carry))
 
-# # takes 2 lists goes through them and adds them together in segments if the string is bigger then the
-# # segment it will return it as a carry to add onto the next
-# def mathAdd(list1, list2, carry, index):
-#     # print("carry is ",carry)
-#     adding = int(list1[index])+int(list2[index])+int(carry)
+            return list
+        else:
+          return mathAdd(list, list1, list2, carry, index+1)
+    else:
+        list.append(add)
 
-#     add = str(adding)
-#     if(add == "0"):
-#         add = "0000"
+        carry = 0
 
-#     # check if the 2 segments added together is bigger then the 4 digit segment allowed if so add the last digit to carry
-#     if(len(add) > 4):
-#         carry = int(add[:1])
-#         # print( "carry is =",carry)
-#         final.append(add[1:])
-#         if (index == len(list1)-1):
-#             if(carry):
-#                 final.append(carry)
-#             return 0
-#         mathAdd(list1, list2, carry, index+1)
-#     else:
-#         final.append(add)
+        # print("carry is", carry)
+        if (index == len(list1)-1):
+            return list
+        else:
+           return mathAdd(list, list1, list2, carry, index+1)
 
-#         carry = 0
-
-#         # print("carry is", carry)
-#         if (index == len(list1)-1):
-#             return 0
-#         mathAdd(list1, list2, carry, index+1)
-
-
-# # takes the 4 digit segments and combinds them again
-# def assemble(index):
-#     # print ("index is ",index)
-#     # print(" val is ",final[index])
-#     if (index == len(final)-1):
-#         # print("about to quit")
-#         return final[index]
-#     return assemble(index+1)+final[index]
-
-
-# # takes a string and breaks it into 4 digit segments in reversed order
-# def formlists(list, num):
-#     if(len(num) < 4):
-#         list.append(num)
-#         return" "
-#     list.append(num[-4:])
-#     formlists(list, num[:-4])
-
-
-# # x="199991111"
-# # print(x[-4:])            [2]
-# # 1000 +9000=  [1][0000][0000]
-# formlists(list1, "100001111")
-# print(list1)
-# formlists(list2, "111110000")
-# print(list2)
-# mathAdd(list1, list2, 0, 0)
-# print("=")
-# print(assemble(0))
-
-# setlists()
-# print("should be 6")
-# formlists(list1, "1000")
-# print(list1)
-# formlists(list2, "9000")
-# print(list2)
-# mathAdd(list1, list2, 0, 0)
-# print("=")
+# takes a string and breaks it into 4 digit segments in reversed order
+def formlists(list, num):
+    if(len(num) < 4):
+        if(len(num) > 0):
+            list.append(num)
+        return list
+    else:
+        list.append(num[-4:])
+        return formlists(list, num[:-4])
 
 def addingZeros(index, zeroes):
-       
-        if(index==0):
-            return zeroes
-        else:
-            zeroes +="0"
-            return addingZeros(index-1,zeroes)
+
+    if(index == 0):
+        return zeroes
+    else:
+        zeroes += "0"
+        return addingZeros(index-1, zeroes)
 
     # print(assemble(0))
 
@@ -119,9 +87,10 @@ def assemble(index, list):
         return int(list[index])
     return assemble(index+1, list)+int(list[index])
 
+
+# def addAll(list):
+#     return list
+
 print("5223333333310000000*500401=")
-print(assemble(0, mathMult("52233333333100000001444444444444444444444444444444444444444444444444444444", "500555555559999999999599959996999899949996999399929992994999599695994939929299999999999999999999999999999986986876855401", 0, list())))
+print( mathMult("52233333333100000001444444444444444444444444444444444444444444444444444444", "500555555559999999999599959996999899949996999399929992994999599695994939929299999999999999999999999999999986986876855401", 0, list()))
 # print(addingZeros(5,""))
-
-
-# def mathMult(list1,list2,index):
